af9c41f4304145531519e6a0144d76f03098851c
shoooone <pride.a.s.w.12.01@gmail.com>
2021-02-19 11:05:01 +0900

    勝者判定

--- src/index.js
+++ src/index.js
@@ -31,17 +31,23 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
+    const squares = [...this.state.squares];
+    if(calculateWinner(squares) || squares[i]) return;
     const {count} = this.state;
     const mark = count % 2 === 0 ? '○' : '×';
-    const squares = [...this.state.squares];
     squares[i] = mark;
     this.setState({squares, count: count + 1});
 
   }
 
   render() {
-    const status = `Next player: ${this.state.count % 2 === 0 ? '○' : '×'}`;
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.count % 2 === 0 ? '○' : '×'}`;
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -87,3 +93,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
